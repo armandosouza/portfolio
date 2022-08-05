45e465a52cc2fb54800c280eff13f5319b9e5d11
Armando Souza <armandosouzafalzi@gmail.com>
2022-08-05 20:42:00 -0300

    Added animate scroll and footer section

---
+++ js/animate.js
@@ -0,0 +1,22 @@
+//Shows burger menu when clicked
+const navbarBurger = document.getElementsByClassName('navbar-burger')[0]
+const navbarMenu = document.getElementById(navbarBurger.dataset.target)
+navbarBurger.addEventListener('click', () => {
+	navbarBurger.classList.toggle('is-active')
+	navbarMenu.classList.toggle('is-active')
+})
+
+//Tooltips - Footer
+const socialFooter = document.getElementsByClassName('foot-social')[0]
+const itemSocial = socialFooter.children[0].children
+for(let i = 0; i < itemSocial.length; i++) {
+	itemSocial[i].addEventListener('mouseover', () => {
+		itemSocial[i].children[1].classList.remove('is-hidden')
+	})
+	itemSocial[i].addEventListener('mouseout', () => {
+		itemSocial[i].children[1].classList.add('is-hidden')
+	})
+}
+
+//Animate on Scroll
+AOS.init()
